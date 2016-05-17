727a137d5e4132891a4bdc65c219100b8d792d7f
Josh Hunt <josh@trtr.co>
2016-05-17 21:25:27 +1000

    Remove comment!

--- src/Routes.js
+++ src/Routes.js
@@ -6,13 +6,6 @@ import Home from './views/Home';
 import About from './views/About';
 import NoMatch from './views/NoMatch';
 
-// If you use React Router, make this component
-// render <Router> with your routes. Currently,
-// only synchronous routes are hot reloaded, and
-// you will see a warning from <Router> on every reload.
-// You can ignore this warning. For details, see:
-// https://github.com/reactjs/react-router/issues/2182
-
 export default function Routes() {
   return (
     <Router history={browserHistory}>
